f52ad7823119011bde3e52b1bfc7a0af5949df7a
Karol <kypacheco07@misena.edu.co>
2022-09-04 00:26:55 -0500

    Creating navbar router

---
+++ heroes/src/design/components/Logo.jsx
@@ -0,0 +1,4 @@
+import logo from "../../assets/logo.jpg";
+export const Logo = () => {
+  return <img src={logo} className="block h-14 w-auto  rounded-lg" />;
+};
--- heroes/src/router/AppRouter.jsx
+++ heroes/src/router/AppRouter.jsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Login } from "../auth/pages/Login";
+import { Navbar } from "../design/components/Navbar";
 import { DcPage } from "../superheroes/pages/DcPage";
 import { MarvelPage } from "../superheroes/pages/MarvelPage";
 
 export const AppRouter = () => {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="marvel" element={<MarvelPage />} />
         <Route path="dc" element={<DcPage />} />
